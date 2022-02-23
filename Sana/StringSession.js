85d09ae37a139a395f77e36fc5e0e66812022f52
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-02-23 15:05:41 +0530

    Create StringSession.js

--- Sana/StringSession.js
+++ Sana/StringSession.js
@@ -18,7 +18,7 @@ class StringSession {
 
     deCrypt(string = undefined) {
 
-        if ('AMRUSER_CODE' in process.env && string === undefined) {
+        if ('AMRU_CODE' in process.env && string === undefined) {
 
             string = process.env.STRING_SESSION;
 
@@ -46,7 +46,7 @@ class StringSession {
 
     createStringSession(dict) {
 
-        return 'Sana;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Amru;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
 
     }
 
