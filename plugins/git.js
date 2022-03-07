ddd5a62102e2cd3cba15c4a3b426f5f958b69de7
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-03-07 21:39:31 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -15,35 +15,17 @@ Sana.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message,
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.imgur.com/71Jt8W3.jpg";
+    r_text[0] =
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `◄━⦁💝 𝐒𝐀𝐍𝐀 𝐌𝐖𝐎𝐋💝⦁━►
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption:
 
 
-*Oᴡɴᴇʀ number Amruthesh* :https://wa.me/917025631103.
-            
-
-*Oᴡɴᴇʀ number Abu* :https://wa.me/917025994178.
-
-
-*Wʜᴀᴛsᴀᴘᴘ Gʀᴏᴜᴘ Sᴀɴᴀ Mᴡᴏʟ* :https://chat.whatsapp.com/DnHJu25Ccss7zn72nPhL8z.
-
-
-*Gɪᴛ Lɪɴᴋs* : https://github.com/AMRUSIR/AMRU_SER-V1.
-
-
-*Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀɴᴅs* :https://github.com/AMRUSIR/Amru-Media/tree/main/bgm.
-
-
-*sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀɴᴅs* :https://github.com/AMRUSIR/Amru-Media/tree/main/stickers.
-
-☢️AMRU_SER-V1☢️
-■□ ~💙 AMRU ❤️I💙 ABU ❤️~■□ 
+*Gɪᴛ Lɪɴᴋs* : https://github.com/AMRUSIR/AMRU_SER-V1
 
 `}) 
 
